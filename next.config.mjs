0869d89c84e73ed47d8c4d7bb79a2ecc0853ce29
Chenyme <1595577976@qq.com>
2025-06-08 12:31:48 +0800

    fix(routing): correct page redirection error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import nextra from 'nextra'
  
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  // ... Add Nextra-specific options here
+  contentDirBasePath: '/docs'
 })
  
 // Export the final Next.js config with Nextra included
