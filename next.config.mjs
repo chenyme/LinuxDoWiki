b0f28127c613852d088ddb805ac0e2f074d6eee0
Chenyme <1595577976@qq.com>
2025-06-08 12:49:27 +0800

    fix(Path): change the base path

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import nextra from 'nextra'
  
 // Set up Nextra with its configuration
 const withNextra = nextra({
-  contentDirBasePath: '/docs'
+  contentDirBasePath: '/'
 })
  
 // Export the final Next.js config with Nextra included
